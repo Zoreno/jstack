ee76ce481f98b883b4d967579076a898971c1ef4
Joakim Bertils <metallica317@spray.se>
2020-11-22 22:12:50 +0100

    Added ARP cache debug printing.

--- src/arp/arp_cache.c
+++ src/arp/arp_cache.c
@@ -6,6 +6,65 @@
 
 static arp_cache_entry_t arp_cache[ARP_CACHE_LEN];
 
+static const char *hw_type_to_string(uint16_t hw_type)
+{
+    switch (hw_type)
+    {
+    case ARP_ETHERNET:
+        return "Ethernet";
+    default:
+        break;
+    }
+
+    return "Unknown";
+}
+
+static void ip_to_string(char *str, uint32_t ip)
+{
+    sprintf(str, "%i.%i.%i.%i",
+            ip & 0xFF,
+            (ip >> 8) & 0xFF,
+            (ip >> 16) & 0xFF,
+            (ip >> 24) & 0xFF);
+}
+
+static void mac_to_string(char *str, unsigned char *mac)
+{
+    sprintf(str, "%02x:%02x:%02x:%02x:%02x:%02x",
+            mac[0],
+            mac[1],
+            mac[2],
+            mac[3],
+            mac[4],
+            mac[5]);
+}
+
+static void print_arp_cache()
+{
+    arp_cache_entry_t *entry;
+
+    for (int i = 0; i < ARP_CACHE_LEN; ++i)
+    {
+        entry = &arp_cache[i];
+
+        if (entry->state != ARP_FREE)
+        {
+            char ip[32];
+            char mac[32];
+
+            ip_to_string(ip, entry->src_ip);
+            mac_to_string(mac, entry->src_mac);
+
+            log_debug("Entry %2i: hw_type=%s(0x%04x) ip=%s, mac=%s",
+                      i,
+                      entry->hw_type,
+                      hw_type_to_string(entry->hw_type),
+                      ip,
+                      mac);
+        }
+    }
+}
+
 void arp_cache_init()
 {
     log_info("Initializing ARP cache");
@@ -30,6 +89,8 @@ int arp_cache_insert_entry(arp_header_t *header, arp_ipv4_t *data)
             entry->hw_type = header->hw_type;
             entry->src_ip = data->src_ip;
             memcpy(entry->src_mac, data->src_mac, sizeof(entry->src_mac));
+
+            print_arp_cache();
 
             return 0;
         }
@@ -90,6 +151,8 @@ int arp_cache_get_size()
 
 unsigned char *arp_cache_lookup(uint32_t ip_addr)
 {
+    print_arp_cache();
+
     arp_cache_entry_t *entry;
 
     for (int i = 0; i < ARP_CACHE_LEN; ++i)
